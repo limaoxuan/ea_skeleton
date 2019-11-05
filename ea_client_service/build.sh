629f797af4adc2a5ecf3227f8c4d724a7a2aeb92
Xiangkui Guo <bruce1988cn@gmail.com>
2019-11-05 00:55:35 -0600

    nginx cfg

--- ea_client_service/build.sh
+++ ea_client_service/build.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+kubectl delete -n default deployment ea-client-deployment
+kubectl delete -n default service ea-client-service
+kubectl apply -f ea_client_service.yml
+OUTPUT="$(kubectl describe node minikube|grep InternalIP)"
+stringarray=($OUTPUT)
+echo "${stringarray[1]}"
+open http://${stringarray[1]}:30001/login.html #if fail, refresh the page again
