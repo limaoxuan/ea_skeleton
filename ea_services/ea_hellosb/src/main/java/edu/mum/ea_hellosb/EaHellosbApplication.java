47356b9823a700ed4b627c851a8eee0ca125b148
Xiangkui Guo <bruce1988cn@gmail.com>
2019-11-05 11:42:40 -0600

    ea services

---
+++ ea_services/ea_hellosb/src/main/java/edu/mum/ea_hellosb/EaHellosbApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.ea_hellosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaHellosbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaHellosbApplication.class, args);
+    }
+
+}
---
+++ ea_services/ea_hellosb/src/test/java/edu/mum/ea_hellosb/EaHellosbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.ea_hellosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaHellosbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
